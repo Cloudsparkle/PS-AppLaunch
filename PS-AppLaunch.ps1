a811221fbdee07cf134eeb3f7ebfa9494477e9bd
Bart Jacobs <60138978+Cloudsparkle@users.noreply.github.com>
2021-06-28 12:01:02 +0200

    0.1

---
+++ PS-AppLaunch.ps1
@@ -0,0 +1,100 @@
+param ($PublishedApp)
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.PARAMETER PublishedApp
+
+.INPUTS
+
+.OUTPUTS
+
+.NOTES
+  Version:        1.0
+  Author:         Bart Jacobs - @Cloudsparkle
+  Creation Date:
+  Purpose/Change:
+ .EXAMPLE
+  None
+#>
+
+# Initialize variables
+$AppCommandLine = "c:\NAV\finsqlr2.bat"
+$AppCommandLineArgs = '"SERVERNAME=L-EMEA-SQLPOTH2,database=NAV_O_SADBEL,ID=%ZUPFILES%\POTH2\BE-KI Sadbel - %USERNAME%.ZUP,ntauthentication=0,company=KLS -KTN Logistic Systems"'
+$currentDir = [System.AppDomain]::CurrentDomain.BaseDirectory.TrimEnd('\')
+if ($currentDir -eq $PSHOME.TrimEnd('\'))
+{
+  $currentDir = $PSScriptRoot
+}
+# Currentdir is the actual running directory
+
+# Load libraries
+[System.Reflection.Assembly]::LoadWithPartialName('presentationframework') | Out-Null
+[System.Reflection.Assembly]::LoadFrom($currentdir + "\assembly\MahApps.Metro.dll") | Out-Null
+[System.Reflection.Assembly]::LoadFrom($currentdir + '\assembly\System.Windows.Interactivity.dll') | Out-Null
+
+#The splash-screen
+$hash = [hashtable]::Synchronized(@{})
+$runspace = [runspacefactory]::CreateRunspace()
+$runspace.ApartmentState = "STA"
+$runspace.ThreadOptions = "ReuseThread"
+$runspace.Open()
+$runspace.SessionStateProxy.SetVariable("hash",$hash)
+$Pwshell = [PowerShell]::Create()
+
+$Pwshell.AddScript({
+$xml = [xml]@"
+     <Window
+	xmlns:Controls="clr-namespace:MahApps.Metro.Controls;assembly=MahApps.Metro"
+	xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+	xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+	x:Name="WindowSplash" Title="SplashScreen" WindowStyle="None" WindowStartupLocation="CenterScreen"
+	Background="Red" ShowInTaskbar ="true"
+	Width="600" Height="350" ResizeMode = "NoResize" >
+
+	<Grid>
+		<Grid.RowDefinitions>
+            <RowDefinition Height="70"/>
+            <RowDefinition/>
+        </Grid.RowDefinitions>
+
+		<Grid Grid.Row="0" x:Name="Header" >
+			<StackPanel Orientation="Horizontal" HorizontalAlignment="Left" VerticalAlignment="Stretch" Margin="20,10,0,0">
+				<Label Content="KTN App Launcher" Margin="0,0,0,0" Foreground="White" Height="50"  FontSize="30"/>
+			</StackPanel>
+		</Grid>
+        <Grid Grid.Row="1" >
+		 	<StackPanel Orientation="Vertical" HorizontalAlignment="Center" VerticalAlignment="Center" Margin="5,5,5,5">
+				<Label x:Name = "LoadingLabel"  Foreground="White" HorizontalAlignment="Center" VerticalAlignment="Center" FontSize="24" Margin = "0,0,0,0"/>
+				<Controls:MetroProgressBar IsIndeterminate="True" Foreground="White" HorizontalAlignment="Center" Width="350" Height="20"/>
+			</StackPanel>
+        </Grid>
+	</Grid>
+
+</Window>
+"@
+
+$reader = New-Object System.Xml.XmlNodeReader $xml
+$hash.window = [Windows.Markup.XamlReader]::Load($reader)
+$hash.LoadingLabel = $hash.window.FindName("LoadingLabel")
+$hash.LoadingLabel.Content= "Getting ready..."
+$hash.window.ShowDialog()
+
+}) | Out-Null
+
+# Launching Splash-screen
+$Pwshell.Runspace = $runspace
+$script:handle = $Pwshell.BeginInvoke()
+#Without 5 seconds of sleep, errors are thrown at closure
+sleep 5
+
+write-host $AppCommandLine $AppCommandLineArgs
+sleep 30
+
+# Closing splash-screen
+$hash.window.Dispatcher.Invoke("Normal",[action]{ $hash.window.close() })
+$Pwshell.EndInvoke($handle) | Out-Null
+$runspace.Close() | Out-Null
+
+exit 0
